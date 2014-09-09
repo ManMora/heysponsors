181d2e09ebc365748dcad027871a4831c46fe339
ManMora <manu11724@gmail.com>
2014-09-09 00:32:37 -0500

    No two DB´s and ready for git flow.

--- heysponsors/settings.py
+++ heysponsors/settings.py
@@ -63,11 +63,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    
-    'crawlerdb': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'crawlerdb.sqlite3'),
     }
 }
 
