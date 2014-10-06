f248f3ebcd2874e449bf9d966c1bc4d419206ad9
ManMora <manu11724@gmail.com>
2014-10-05 21:14:39 -0500

    Crawler basic functionality ready! make a post with url attribute and the desired url to be crawled as value to http://routetoproject/crawler/gettags

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+import urllib
+import json
+from bs4 import BeautifulSoup
+import requests
+from django.http import HttpResponse
+from django.core.context_processors import csrf
 
-# Create your views here.
+
+class result():
+    def __init__(self, name=None, logo=None, tel=None, email=None, direccion=None):
+        self.name = name
+        self.logo = logo
+        self.tel = tel
+        self.email = email
+        self.direccion = direccion
+
+
+def gettags(request):
+
+    if request.POST != None and request.POST.url != None:
+        htmlfile = urllib.urlopen(request.POST.url)
+        soup = BeautifulSoup(htmlfile)
+        results = []
+        vcards = soup.find_all("li", class_="vcard")
+        for x in vcards:
+            if x.span['class'] != None and x.span['class'] == 'street-address':
+                addr = x.span.string
+            else:
+                addr = None
+            a = x.find_all("span",class_="tel")
+            tel = a[0].string
+
+            b = x.find_all("span",class_="street-address")
+            if len(b)>0:
+               addr = b[0].string
+
+            if x.img != None:
+               img = x.img['src']
+            else:
+               img = None
+
+            results.append(result(x.h3.a.string,img,tel,None,addr))
+        return HttpResponse(json.dumps([dict(result=x.__dict__) for x in results]), content_type="application/json")
+    return HttpResponse(json.dumps(None), content_type="application/json")
+
